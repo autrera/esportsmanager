f19c1380d110b82a8e61dbee32eb6cf4f9c805ad
Aldo Utrera <Kugel85@gmail.com>
2012-03-13 17:55:25 -0600

    Cambiando los permisos del cache y agregando el primer mockup de agregar usuarios con cakephp

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Static content controller.
+ *
+ * This file will render views from views/pages/
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       app.Controller
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * Static content controller
+ *
+ * Override this controller by placing a copy in controllers directory of an application
+ *
+ * @package       app.Controller
+ */
+class UsersController extends AppController {
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+	public $name = 'Users';
+
+/**
+ * Default helper
+ *
+ * @var array
+ */
+	public $helpers = array('Html');
+
+/**
+ * This controller does not use a model
+ *
+ * @var array
+ */
+	public $uses = array();
+
+/**
+ * Agrega usuarios
+ *
+ * @param mixed What page to display
+ */
+	public function add() {
+	    if (!empty($this->request->data)) {
+	        // We can save the User data:
+	        // it should be in $this->request->data['User']
+
+	        $user = $this->User->save($this->request->data);
+
+	        // If the user was saved, Now we add this information to the data
+	        // and save the Profile.
+
+	        if (!empty($user)) {
+	            // The ID of the newly created user has been set
+	            // as $this->User->id.
+	            $this->request->data['Profile']['user_id'] = $this->User->id;
+
+	            // Because our User hasOne Profile, we can access
+	            // the Profile model through the User model:
+	            $this->User->Profile->save($this->request->data);
+	        }
+	    }
+	}
+}

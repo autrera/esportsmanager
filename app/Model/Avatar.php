1e7b5f2eda98e911083591777c5af9537ba8bf78
Aldo Utrera <Kugel85@gmail.com>
2012-03-28 13:59:23 -0600

    Agregamos lógica al modelo de Avatar, se agregó la función que formatea el request data para permitir un ciclo de guardados al mismo modelo, también se agregó la función que retorna el directorio donde se almacenarán los archivos enviados

--- app/Model/Avatar.php
+++ app/Model/Avatar.php
@@ -50,4 +50,38 @@ class Avatar extends AppModel {
 			'order' => ''
 		)
 	);
+
+/**
+ * Vamos a formar varios mini request data y meterlos dentro de otro array
+ * así podemos ciclar el proceso por cada archivo y guardar un archivo a la vez
+ * Podriamos llamar al método saveMany, pero no creo que sea lo mas conveniente
+ *
+ * @param array data El request data enviado del form
+ * @return array newData El multi request data para ciclar el guardado
+ */
+	public function formatearMultiUpload($data){
+		if (! empty($data)){
+			$newData  = array();	
+			$games_id = $data['Avatar']['games_id'];
+			foreach ($data['Avatar'] as $key => $avatar){
+				if (is_numeric($key)){
+					$newData[$key] = array($this->alias => $avatar);
+					$newData[$key][$this->alias]['games_id'] = 
+						$games_id;
+				}
+			}
+			return $newData;
+		}
+	}
+
+/**
+ * Retornamos el path explicito de donde almacenaremos las imagenes
+ *
+ * @param none
+ * @return String El path a la carpeta de alamacenamiento
+ */
+	public function getStorageDir(){
+		return ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS . 'uploads' . DS . 'avatars' . DS;
+	}
+
 }

f5ca018f91b03a1c6fd8ad93c8085fb130cc1947
Aldo Utrera <Kugel85@gmail.com>
2012-04-05 14:00:08 -0500

    Agregando los estilos personalizados que usaremos para estilizar lo que no podamos con bootstrap, agregando avances en la estructura del dashboard

---
+++ app/webroot/dashboard.php
@@ -0,0 +1,122 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="css/bootstrap.css" rel="stylesheet"> 
+        <link href="css/default.css" rel="stylesheet"> 
+    </head>
+    <body>
+        
+        <script language = "javascript" type = "text/javascript" 
+            src = "js/bootstrap.js" >
+        </script>
+
+        <div class = "container">
+            <div id = "header">
+                <div class = "row">
+                    <div class = "span7">
+                        <div class = "alert alert-info">
+                            <strong>Welcome to Team Quetzal!</strong>
+                        </div>
+                    </div>
+                    <div class = "cont-login-header span5">
+                        <form class = "form-inline">
+                            <input type = "text" class = "input-small" 
+                                placeholder = "Email">
+                            <input type = "password" class = "input-small" 
+                                placeholder = "Password">
+                            <label class="checkbox">
+                                <input type="checkbox"> Remember me
+                            </label>
+                            <button type = "submit" class = "btn">
+                                Sign in
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div class = "row">
+                    <div class = "span12">
+                        <ul class = "thumbnails">
+                            <li class = "span6">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/560x166" 
+                                    alt = "">
+                                </a>
+                            </li>
+                            <li class = "span3">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/260x69" 
+                                    alt = "">
+                                </a>
+                            </li>
+                            <li class = "span3">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/260x69" 
+                                    alt = "">
+                                </a>
+                            </li>
+                            <li class = "span3">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/260x69" 
+                                    alt = "">
+                                </a>
+                            </li>
+                            <li class = "span3">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/260x69" 
+                                    alt = "">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="navbar">
+                    <div class="navbar-inner">
+                        <div class="container">
+                            <ul class="nav">
+                                <li class="active">
+                                    <a href="#">Home</a>
+                                </li>
+                                <li><a href="#">Link</a></li>
+                                <li><a href="#">Link</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id = "background">
+                <div class = "row">
+                    <div class = "span8">
+                        <ul class = "thumbnails">
+                            <li class = "span8">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/770x300" 
+                                    alt = "">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class = "span4">
+                        <ul class = "thumbnails">
+                            <li class = "span4">
+                                <a href = "#" class = "thumbnail">
+                                  <img src = "http://placehold.it/360x140" 
+                                    alt = "">
+                                </a>
+                                <p>Nombre del video de arriba</p>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>Nombre de otro video</li>
+                            <li>Nombre de otro video</li>
+                            <li>Nombre de otro video</li>
+                            <li>Nombre de otro video</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>

b00856adc9d71e06ca13a625a881da9a1d606053
Aldo Utrera <Kugel85@gmail.com>
2012-04-25 12:52:42 -0500

    Commit inicial del modelo que unifica los usuarios con sus streams, un usuario puede tener infinito número de streams, pensando en que posiblemente transmitan desde distintos proveedores al mismo tiempo, o tengan un canal para cada juego, etc.

---
+++ app/Model/StreamsUser.php
@@ -0,0 +1,119 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * StreamsUser Model
+ *
+ * @property Users $Users
+ * @property Streams $Streams
+ * @property Games $Games
+ */
+class StreamsUser extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'service_id';
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'service_id' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'users_id' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'streams_id' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'games_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Users' => array(
+			'className' => 'Users',
+			'foreignKey' => 'users_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Streams' => array(
+			'className' => 'Streams',
+			'foreignKey' => 'streams_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Games' => array(
+			'className' => 'Games',
+			'foreignKey' => 'games_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}

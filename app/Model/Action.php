1e29bd7945a38ef5a46078fb45594853f912dd96
Aldo Utrera <Kugel85@gmail.com>
2012-04-30 17:40:49 -0500

    Commit inicial del modulo de acciones

---
+++ app/Model/Action.php
@@ -0,0 +1,74 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Action Model
+ *
+ * @property ActionsRole $ActionsRole
+ * @property ActionsUser $ActionsUser
+ */
+class Action extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * hasAndBelongsToMany associations
+ *
+ * @var array
+ */
+	public $hasAndBelongsToMany = array(
+		'ActionsRole' => array(
+			'className' => 'ActionsRole',
+			'joinTable' => 'modules_actions_roles',
+			'foreignKey' => 'action_id',
+			'associationForeignKey' => 'actions_role_id',
+			'unique' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		),
+		'ActionsUser' => array(
+			'className' => 'ActionsUser',
+			'joinTable' => 'modules_actions_users',
+			'foreignKey' => 'action_id',
+			'associationForeignKey' => 'actions_user_id',
+			'unique' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		)
+	);
+
+}

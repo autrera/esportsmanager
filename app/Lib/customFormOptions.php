08c883a455f5ee316c609e7b8e7ab981eff245b1
Aldo Utrera <Kugel85@gmail.com>
2012-05-04 17:28:32 -0500

    Ahora todas las funciones aceptan un array de override, para incluir opciones que se necesiten

--- app/Lib/customFormOptions.php
+++ app/Lib/customFormOptions.php
@@ -3,77 +3,97 @@
 class customFormOptions
 {
     
-    public static function getOptionsDefault($textoAyuda = ''){
-        return array(
-            'div' => array(
-                'class' => 'control-group'
-            ),
-            'label' => array(
-                'class' => 'control-label'
-            ),
-            'class' => 'span6',
-            'between' => '<div class = "controls">',
-            'after' => '<div class = "help-block">'.$textoAyuda.'</div></div>',
-            'error' => array(
-                'attributes' => array(
-                    'wrap' => 'span', 
-                    'class' => array(
-                        'help-block',
-                        'pull-right',
+    public static function getOptionsDefault($textoAyuda = '', 
+        $override = array()
+    ){
+        return array_merge(
+            array(
+                'div' => array(
+                    'class' => 'control-group'
+                ),
+                'label' => array(
+                    'class' => 'control-label'
+                ),
+                'class' => 'span6',
+                'between' => '<div class = "controls">',
+                'after' => '<div class = "help-block">'.$textoAyuda.'</div></div>',
+                'error' => array(
+                    'attributes' => array(
+                        'wrap' => 'span', 
+                        'class' => array(
+                            'help-block',
+                            'pull-right',
+                        )
                     )
+                ),
+            ),
+            $override
+        );
+    }
+
+    public static function getOptionsFile($textoAyuda = '',
+        $override = array()
+    ){
+        return array_merge(
+            array(
+                'div' => array(
+                    'class' => 'control-group'
+                ),
+                'label' => array(
+                    'class' => 'control-label'
+                ),
+                'class' => 'input-file',
+                'between' => '<div class = "controls">',
+                'after' => '<div class = "help-block">'.$textoAyuda.'</div></div>',
+                'error' => array(
+                    'attributes' => array(
+                        'wrap' => 'span', 
+                        'class' => array(
+                            'help-block',
+                            'pull-right',
+                        )
+                    )
+                ),
+                'type' => 'file',
+            ),
+            $override
+        );
+    }
+
+    public static function getOptionsCheckBox($textoAyuda = '',
+        $override = array()
+    ){
+        return array_merge(
+            array(
+                'div' => array(
+                    'class' => 'control-group'
+                ),
+                'label' => array(
+                    'class' => 'control-label'
+                ),
+                'class' => '',
+                'before' => '<div class = "controls"><label class = "checkbox">',
+                'after' => '</label><div class = "help-block">'.$textoAyuda.'</div></div>',
+                'format' => array(
+                    'label', 'before', 'input', 'after'
+                ),
+            ),
+            $override
+        );
+    }
+
+    public static function getOptionsBtnSubmit($btnText = 'Save',
+        $override = array()
+    ){
+        return array_merge(
+            array(
+                'value' => $btnText,
+                'class' => array('btn', 'btn-primary'),
+                'div' => array(
+                    'class' => 'form-actions',
                 )
             ),
-        );
-    }
-
-    public static function getOptionsFile($textoAyuda = ''){
-        return array(
-            'div' => array(
-                'class' => 'control-group'
-            ),
-            'label' => array(
-                'class' => 'control-label'
-            ),
-            'class' => 'input-file',
-            'between' => '<div class = "controls">',
-            'after' => '<div class = "help-block">'.$textoAyuda.'</div></div>',
-            'error' => array(
-                'attributes' => array(
-                    'wrap' => 'span', 
-                    'class' => array(
-                        'help-block',
-                        'pull-right',
-                    )
-                )
-            ),
-            'type' => 'file',
-        );
-    }
-
-    public static function getOptionsCheckBox($textoAyuda = ''){
-        return array(
-            'div' => array(
-                'class' => 'control-group'
-            ),
-            'label' => array(
-                'class' => 'control-label'
-            ),
-            'class' => '',
-            'before' => '<div class = "controls"><label class = "checkbox">',
-            'after' => '</label><div class = "help-block">'.$textoAyuda.'</div></div>',
-            'format' => array(
-                'label', 'before', 'input', 'after'
-            ),
-        );
-    }
-
-    public static function getOptionsBtnSubmit(){
-        return array(
-            'value' => 'Submit',
-            'class' => array('btn', 'btn-primary', 'btn-large'),
-            'div' => array(
-                'class' => 'form-actions',
-            )
+            $override
         );
     }
 }

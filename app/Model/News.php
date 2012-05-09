71237c9e6bf93dc2dbcec027a0a557943ea555fe
Aldo Utrera <Kugel85@gmail.com>
2012-05-09 17:11:47 -0500

    Se agregó la validación en el modelo de games_id, un campo nuevo en la bd

--- app/Model/News.php
+++ app/Model/News.php
@@ -44,6 +44,16 @@ class News extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'games_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
@@ -57,6 +67,13 @@ class News extends AppModel {
 		'Users' => array(
 			'className' => 'Users',
 			'foreignKey' => 'users_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+		),
+		'Games' => array(
+			'className' => 'Games',
+			'foreignKey' => 'games_id',
 			'conditions' => '',
 			'fields' => '',
 			'order' => '',

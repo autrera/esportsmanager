8741aa7e4532fd1a99797b39e47e9396861beb8d
Aldo Utrera <Kugel85@gmail.com>
2012-05-10 02:26:49 -0500

    Se quitaron los avatars_id, como ya se tiene una imagen, los avatares no serán necesarios

--- app/Model/Profile.php
+++ app/Model/Profile.php
@@ -78,16 +78,6 @@ class Profile extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'avatars_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
@@ -112,12 +102,5 @@ class Profile extends AppModel {
 			'fields' => '',
 			'order' => ''
 		),
-		'Avatars' => array(
-			'className' => 'Avatars',
-			'foreignKey' => 'avatars_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
 	);
 }

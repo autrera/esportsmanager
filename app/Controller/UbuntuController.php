26cc4134e3a7bb6422b5155f37e718d4d159d576
Aldo Utrera <Kugel85@gmail.com>
2012-05-30 18:00:02 -0500

    Fixeando direcciones y variables

--- app/Controller/UbuntuController.php
+++ app/Controller/UbuntuController.php
@@ -4,34 +4,50 @@
 App::import('Vendor', 'OAuth/OAuthClient');
 
 class UbuntuController extends AppController {
+
+    public $render = false;
+
+    public $autoRender = false;
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+        $this->Auth->allow('callback'); 
+    }
+
     public function index() {
+        $callback = 'http://teamquetzal.com/esportsmanager/ubuntu/callback';
         $client = $this->createClient();
-        $requestToken = $client->getRequestToken('https://one.ubuntu.com/oauth/request/', 'http://' . $_SERVER['HTTP_HOST'] . '/example/callback');
+        $requestToken = $client->getRequestToken('https://api.dropbox.com/1/oauth/request_token', $callback);
 
         if ($requestToken) {
             $this->Session->write('ubuntu_request_token', $requestToken);
-            $this->redirect('https://one.ubuntu.com/auth/login/?next=/oauth/authorize/=' . $requestToken->key);
+            $this->redirect('https://www.dropbox.com/1/oauth/authorize?oauth_token=' . $requestToken->key . '&oauth_callback=' . $callback);
         } else {
             // an error occured when obtaining a request token
         }
     }
 
     public function callback() {
-        $requestToken = $this->Session->read('twitter_request_token');
+        $requestToken = $this->Session->read('ubuntu_request_token');
         $client = $this->createClient();
-        $accessToken = $client->getAccessToken('https://one.ubuntu.com/oauth/access/', $requestToken);
+        $accessToken = $client->getAccessToken('https://api.dropbox.com/1/oauth/access_token', $requestToken);
 
         if ($accessToken) {
             echo "<pre>";
             print_r($accessToken);
             echo "</pre>";
-            $client->post($accessToken->key, $accessToken->secret, 'https://api.twitter.com/1/statuses/update.json', array('status' => 'hello world!'));
+            // $client->post($accessToken->key, $accessToken->secret, 'https://api.twitter.com/1/statuses/update.json', array('status' => 'hello world!'));
         }
     }
 
     private function createClient() {
-        return new OAuthClient('ubuntuone', 'hammertime');
+        return new OAuthClient('rvob0qty1562swf', 'g2a6f0lyermai8s');
     }
+
+    public $credentials = array(
+        'key' => '7dg4vey4t9rsf6s',
+        'secret' => 'a1zc65o1hrjz9bq'
+    );
 }
 
 ?>

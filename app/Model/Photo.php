0c25272071504f33382686923d891128aeb56864
Aldo Utrera <Kugel85@gmail.com>
2012-06-04 13:17:36 -0500

    Removiendo la regla del nombre, ahora es opcional

--- app/Model/Photo.php
+++ app/Model/Photo.php
@@ -13,16 +13,6 @@ class Photo extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'name' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				'message' => 'This field can\'t be empty',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'url' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),

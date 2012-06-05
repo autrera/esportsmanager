6ef69db039bd3fe04795ddc8ce46e94b61dc8171
Aldo Utrera <Kugel85@gmail.com>
2012-06-05 13:28:26 -0500

    Cambiando la info de la DB, para tunelear a pagoda

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,10 +4,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
-		'port' => 3306,
-		'login' => 'root',
-		'password' => '',
-		'database' => 'mydb',
+		'host' => '127.0.0.1',
+		'port' => 3307,
+		'login' => 'lavette',
+		'password' => 'JYnCWxo7',
+		'database' => 'youlanda',
 	);
 }

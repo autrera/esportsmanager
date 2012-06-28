6e0d2f3b64ca318a2268eac3ed894ee8966989f6
Aldo Utrera <Kugel85@gmail.com>
2012-06-28 10:23:09 -0500

    Haciendo el efecto de hover ahora por javascript

--- app/webroot/js/initializers.js
+++ app/webroot/js/initializers.js
@@ -17,6 +17,25 @@
 			}
 		});
 
+		$(".enlarger").hover(function(){
+			$(this).animate({
+                'z-index': 999,
+                'min-width': '120px',
+                'height': '80px',
+                'left': '-10px',
+                'top': '-10px'
+			}, 50, 'swing');
+		}, function (){
+			$(this).animate({
+				'z-index': 998,
+				'left': '0px',
+				'top': '0px',
+				'height': '60px',
+				'min-width': '100px',
+				'width': '100px'
+			}, 50, 'swing');
+		});
+
     });
 
 }( window.jQuery );

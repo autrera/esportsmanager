2304acb79b5ecf055ddd373e7b1a3e2ce5e04ee0
Aldo Utrera <Kugel85@gmail.com>
2012-09-04 12:30:28 -0500

    Seteando las credenciales de la base de datos

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,10 +4,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.0.0.1',
-		'port' => 3307,
-		'login' => 'lavette',
-		'password' => 'JYnCWxo7',
-		'database' => 'youlanda',
+		'host' => 'tunnel.pagodabox.com',
+		'port' => 3306,
+		'login' => 'delilah',
+		'password' => 'BJWPQ4Gr',
+		'database' => 'albina',
 	);
 }

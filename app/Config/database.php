4a6a9e5b82d1d56d03e7b7df6668655a6bbeef35
Aldo Utrera <Kugel85@gmail.com>
2012-09-04 13:48:06 -0500

    Purgando el cache, agregando gitignores en las carpetas de tmp al estilo laravel

--- app/Config/database.php
+++ app/Config/database.php
@@ -4,8 +4,8 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'tunnel.pagodabox.com',
-		'port' => 3306,
+		'host' => '127.0.0.1',
+		'port' => 3307,
 		'login' => 'delilah',
 		'password' => 'BJWPQ4Gr',
 		'database' => 'albina',

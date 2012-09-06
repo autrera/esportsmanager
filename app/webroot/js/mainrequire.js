6d4367db1de98bba15e16f211cc26d6d9b112719
Aldo Utrera <Kugel85@gmail.com>
2012-09-06 10:46:50 -0500

    Agregando lazyload para las imagenes, comentando casi todo el archivo ya que ahora no estamos usando el código

--- app/webroot/js/mainrequire.js
+++ app/webroot/js/mainrequire.js
@@ -1,46 +1,48 @@
 require([
     "jquery",
-    "bootstrap.min",
-    "facebook",
-    "jquery.fancybox.pack",
-    "jquery.fancybox-buttons",
-    "jquery.fancybox-media",
-    "jquery.fancybox-thumbs"
+    // "bootstrap.min",
+    "lazyload.min"
+    // "jquery.fancybox.pack",
+    // "jquery.fancybox-buttons",
+    // "jquery.fancybox-media",
+    // "jquery.fancybox-thumbs"
 ], function($) {
     $(function() {
 
-        $('.carousel').carousel({
-        });
+        $("img.lazy").lazyload();
 
-        $("a[rel=popover]").popover();
+        // $('.carousel').carousel({
+        // });
 
-        $(".fancybox").fancybox({
-            autoSize: false,
-            height: '90%',
-            width: 640,
-            afterShow: function(){
-                this.resize();
-            }
-        });
+        // $("a[rel=popover]").popover();
 
-        $(".enlarger").hover(function(){
-            $(this).animate({
-                'z-index': 999,
-                'min-width': '120px',
-                'height': '80px',
-                'left': '-10px',
-                'top': '-10px'
-            }, 50, 'swing');
-        }, function (){
-            $(this).animate({
-                'z-index': 998,
-                'left': '0px',
-                'top': '0px',
-                'height': '60px',
-                'min-width': '100px',
-                'width': '100px'
-            }, 50, 'swing');
-        });
+        // $(".fancybox").fancybox({
+        //     autoSize: false,
+        //     height: '90%',
+        //     width: 640,
+        //     afterShow: function(){
+        //         this.resize();
+        //     }
+        // });
+
+        // $(".enlarger").hover(function(){
+        //     $(this).animate({
+        //         'z-index': 999,
+        //         'min-width': '120px',
+        //         'height': '80px',
+        //         'left': '-10px',
+        //         'top': '-10px'
+        //     }, 50, 'swing');
+        // }, function (){
+        //     $(this).animate({
+        //         'z-index': 998,
+        //         'left': '0px',
+        //         'top': '0px',
+        //         'height': '60px',
+        //         'min-width': '100px',
+        //         'width': '100px'
+        //     }, 50, 'swing');
+        // });
 
     });
 });
